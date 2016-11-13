d7f77ce4ff456502890a2ebfb56208650583cf14
Cameron <code@cameronford.net>
2016-11-13 13:21:10 +1100

    Length input with prompt and update

--- Main.elm
+++ Main.elm
@@ -2,7 +2,9 @@ import Html.App exposing (beginnerProgram)
 import Html exposing (Html, div, text ,input)
 import Html.Attributes exposing (placeholder)
 import Html.Events exposing (onInput)
+import String exposing (toInt)
 
+main : Program Never
 main = Html.App.beginnerProgram
     { model = model
     , view = view
@@ -30,13 +32,15 @@ view model =
         []
         [ div
             []
-            [ input
-                [ placeholder (toString model.length)
+            [ div
+                []
+                [ text "What's the length of the word?" ]
+            , input
+                [ placeholder "Enter a number"
                 , onInput ChangeLength
                 ]
                 []
             ]
-        
         ]
 
 
@@ -57,5 +61,12 @@ model =
                  ]
     }
 
-update : a -> Model -> Model
-update msg model = model
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ChangeLength s ->
+            case (toInt s) of
+                Ok i ->
+                    { model | length = i }
+                Err _ ->
+                    model
